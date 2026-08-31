e6e176c42692574e81eb9798c61b36ef6a1e4103
L3CHUGU1T4 <le0carbaj4l@gmail.com>
2026-08-31 12:30:32 -0400

    Update vex to v1.0.1

--- Formula/vex.rb
+++ Formula/vex.rb
@@ -1,17 +1,18 @@
 class Vex < Formula
-    desc "Terminal text editor with syntax highlighting for 17 languages"
-    homepage "https://github.com/L3CHUGU1T4/vex"
-    url "https://github.com/L3CHUGU1T4/vex/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "645207f4da177a982b44e66fcca241528240a9dea447bf6bf61ea7206f719574"
-    license "MIT"
-
-    def install
-      system "make", "CXX=#{ENV.cxx}", "PREFIX=#{prefix}"
-      bin.install "build/vex"
-    end
-
-    test do
-      (testpath/"test.cpp").write("int main() { return 0; }\n")
-      system "#{bin}/vex", "--help"
-    end
+  desc "Terminal text editor with syntax highlighting for 17 languages"
+  homepage "https://github.com/L3CHUGU1T4/vex"
+  url "https://github.com/L3CHUGU1T4/vex/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "6b23b35cd3a951f4e004664872af41ee260b8396ff34cdad6ad8c1d16704d48f"
+  license "MIT"
+          
+  depends_on "make"
+            
+  def install
+    system "make", "CXX=#{ENV.cxx}"
+    bin.install "build/vex"
   end
+                        
+  test do
+    assert_match "vex", shell_output("#{bin}/vex --help", 0)
+  end
+end
